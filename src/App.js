a0a47ec9be44cade4106623c202622613d25bd4c
Ammar Mohammed <ammar-baomeir@jisr.net>
2021-03-01 17:40:18 +0300

    menu design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Header from './components/header/Header';
+import Menu from  './components/menu/Menu';
 
 function App() {
   return (
     <div className="App">
       <Header />
-    
+
+      <div className="middle-page">
+        <Menu />
+        <h1>content</h1>
+      </div>
+
     </div>
   );
 }
