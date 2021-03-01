4cf4518f2a90c1c63b6b750595e11e489f4b8b17
Ammar Mohammed <ammar-baomeir@jisr.net>
2021-03-01 16:25:40 +0300

    header design

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { ReactComponent as MovieFilter  } from '../../assets/icons/movie_filter.svg'
+import '../../styles/header.css'
 
 function Header() {
     return (
-        <div>
-            
+        <div className="header">
+            <div></div>
+            <div><h1>Breaking Bad</h1></div>
+            <div className="logo-div"><MovieFilter /></div>
+
         </div>
     )
 }
