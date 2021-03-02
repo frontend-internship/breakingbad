aeef35398390e32a8fc8c7e18ca17a49a79d6d24
Ammar Mohammed <ammar-baomeir@jisr.net>
2021-03-02 10:55:26 +0300

    settings page design

--- src/containers/settings/Settings.js
+++ src/containers/settings/Settings.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from 'react';
+import '../../styles/settings.css';
 
 function Settings() {
     return (
-        <div>
-            Settings
+        <div className="settings">
+            <h1>Settings</h1>
+            <h2>Profile</h2>
+            <ul className="setting-list">
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+            </ul>
+            <h2>Accessibility</h2>
+            <ul className="setting-list">
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+            </ul>
+            <h2>Themes</h2>
+            <ul className="setting-list">
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+            </ul>
+            <h2>Colors</h2>
+            <ul className="setting-list">
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+            </ul>
+            <h2>Fonts</h2>
+            <ul className="setting-list">
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+                <li><input type="checkbox" /> Lorem ipsum dolor sit amet</li>
+            </ul>
         </div>
     )
 }
