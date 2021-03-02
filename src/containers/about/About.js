f04952361ed69f85f05250c8a93a3695e2a77808
Ammar Mohammed <ammar-baomeir@jisr.net>
2021-03-02 10:15:24 +0300

    home design

--- src/containers/about/About.js
+++ src/containers/about/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../../styles/about.css'
+import image1 from '../../assets/images/image1.jpg';
 
 function About() {
     return (
@@ -10,13 +11,18 @@ function About() {
                 quibusdam sit inventore cum veritatis libero distinctio animi aperiam totam quos minima,
                 ducimus assumenda provident ratione. Cumque repudiandae quisquam iure cum aliquid? Corporis, accusamus debitis.
                 <br />
+            </p>
+
+                <img src={image1} alt="" />
+
+             <p>
                 <br />
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat eligendi neque in, earum harum nesciunt,
                 quibusdam sit inventore cum veritatis libero distinctio animi aperiam totam quos minima,
                 ducimus assumenda provident ratione. Cumque repudiandae quisquam iure cum aliquid? Corporis, accusamus debitis.
                 <br />
                 <br />
-
+                
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat eligendi neque in, earum harum nesciunt,
                 quibusdam sit inventore cum veritatis libero distinctio animi aperiam totam quos minima,
                 ducimus assumenda provident ratione. Cumque repudiandae quisquam iure cum aliquid? Corporis, accusamus debitis.
--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,9 +1,64 @@
 import React from 'react'
 
+
+
+import image2 from '../../assets/images/image2.jpg';
+import image3 from '../../assets/images/image3.jpg';
+import image4 from '../../assets/images/image4.jpg';
+
+import '../../styles/home.css'
+
 function Home() {
     return (
-        <div>
-            Home
+        <div className="home">
+            <h1>BREAKING BAD</h1>
+            <img src={image2} alt="" />
+            <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat eligendi neque in, earum harum nesciunt,
+            quibusdam sit inventore cum veritatis libero distinctio animi aperiam totam quos minima,
+            ducimus assumenda provident ratione. Cumque repudiandae quisquam iure cum aliquid? Corporis, accusamus debitis.
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat eligendi neque in, earum harum nesciunt,
+            quibusdam sit inventore cum veritatis libero distinctio animi aperiam totam quos minima,
+            ducimus assumenda provident ratione. Cumque repudiandae quisquam iure cum aliquid? Corporis, accusamus debitis.
+            <br />
+            <br />
+
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat eligendi neque in, earum harum nesciunt,
+            quibusdam sit inventore cum veritatis libero distinctio animi aperiam totam quos minima,
+            ducimus assumenda provident ratione. Cumque repudiandae quisquam iure cum aliquid? Corporis, accusamus debitis.
+            <br />
+            <br />
+            </p>
+
+            <div style={{display: 'flex', justifyContent: 'space-around'}}>
+                <img src={image3} alt="" />
+                <img src={image4} alt="" />
+            </div>
+            <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat eligendi neque in, earum harum nesciunt,
+            quibusdam sit inventore cum veritatis libero distinctio animi aperiam totam quos minima,
+            ducimus assumenda provident ratione. Cumque repudiandae quisquam iure cum aliquid? Corporis, accusamus debitis.
+            <br />
+            <br />
+             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat eligendi neque in, earum harum nesciunt,
+            quibusdam sit inventore cum veritatis libero distinctio animi aperiam totam quos minima,
+            ducimus assumenda provident ratione. Cumque repudiandae quisquam iure cum aliquid? Corporis, accusamus debitis.
+            <br />
+            <br />
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat eligendi neque in, earum harum nesciunt,
+            quibusdam sit inventore cum veritatis libero distinctio animi aperiam totam quos minima,
+            ducimus assumenda provident ratione. Cumque repudiandae quisquam iure cum aliquid? Corporis, accusamus debitis.
+            <br />
+            <br />
+
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat eligendi neque in, earum harum nesciunt,
+            quibusdam sit inventore cum veritatis libero distinctio animi aperiam totam quos minima,
+            ducimus assumenda provident ratione. Cumque repudiandae quisquam iure cum aliquid? Corporis, accusamus debitis.
+            <br />
+            <br />
+            </p>
         </div>
     )
 }
