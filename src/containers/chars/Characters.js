911592e1bfdea786bb0617026f9c1716792e976a
Ammar Mohammed <ammar-baomeir@jisr.net>
2021-03-04 10:04:53 +0300

    character list design enhancement and load more is added

--- src/containers/chars/Characters.js
+++ src/containers/chars/Characters.js
@@ -6,14 +6,30 @@ import Loader from '../../components/Loader';
 
 import '../../styles/characters.css';
 
-const getChars = (searchKey, list) => {
+const getChars = (searchKey, list, limit) => {
+    let couter = limit + 1;
     if (searchKey === "") {
-        return list.map(char => <Character data={char} />)
+        return list.map(char => {
+            couter--;
+            if (couter > 0) {
+                return <Character data={char} />
+            } else {
+                return ""
+            }
+        })
+
+
     } else {
         return list.map(char => {
-            if (char.name.toLowerCase().includes(searchKey.toLowerCase())) {
-                return <Character data={char} />
 
+            couter--;
+            if (couter > 0) {
+                if (char.name.toLowerCase().includes(searchKey.toLowerCase()) || char.nickname.toLowerCase().includes(searchKey.toLowerCase()) || char.status.toLowerCase().includes(searchKey.toLowerCase())) {
+                    return <Character data={char} />
+
+                } else {
+                    return ""
+                }
             } else {
                 return ""
             }
@@ -24,6 +40,7 @@ const getChars = (searchKey, list) => {
 function Characters(props) {
 
     const [searchkey, setSearchKey] = useState("");
+    const [limit, setLimit] = useState(24);
 
     useEffect(() => {
         props.GetAllChars()
@@ -32,11 +49,22 @@ function Characters(props) {
     const chars = props.Characters.characters;
 
     return (
-        <div>
-            <div className="search-container"><input type="text" placeholder="search for..." onChange={(e) => setSearchKey(e.target.value)} /> </div>
+        <div style={{ marginBottom: '3rem' }}>
+            <div className="search-container"><input type="text" placeholder="search for name, nickname or status..." onChange={(e) => setSearchKey(e.target.value)} /> </div>
             <div className="cards-container">
-                {props.Characters.fetching ? <Loader /> : getChars(searchkey, chars)}
+                {props.Characters.fetching ? <Loader /> : getChars(searchkey, chars, limit)}
             </div>
+            {
+                chars.length > limit ?
+                    (
+                        props.Characters.fetching ?
+                            ""
+                            :
+                            <div className="load-more" onClick={() => setLimit(limit + 12)}>Load more</div>
+                    )
+                    :
+                    "No More Characters"
+            }
         </div>
     )
 }
